afe263176c2be448df18e51f9dac5634ad325e59
Ivan Song <mousewurx@gmail.com>
2025-03-26 21:36:31 -0700

    Updated the sitename link to homepage and increase font size of <p> on .page-title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
     // </header>
     <header id="header" className="header d-flex align-items-center fixed-top">
       <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" className="logo d-flex align-items-center">
+        <a href="/" className="logo d-flex align-items-center">
           {/* Uncomment the line below if you also wish to use an image logo */}
           {/* <img src="assets/img/logo.png" alt="" /> */}
           <h1 className="sitename">Brad Ahn Law</h1>
