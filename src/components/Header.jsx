3619962f509839b5f767b5a91fc81e6ae2bed84f
Ivan Song <mousewurx@gmail.com>
2025-03-30 01:03:33 -0700

    Updated new logo on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,7 +68,8 @@ const Header = () => {
         <a href="/" className="logo d-flex align-items-center">
           {/* Uncomment the line below if you also wish to use an image logo */}
           {/* <img src="logo-1-600x190.png" alt="Brad Ahn Law Logo" /> */}
-          <img src="logo-5-850x260.png" alt="Brad Ahn Law Logo" />
+          {/* <img src="logo-5-850x260.png" alt="Brad Ahn Law Logo" /> */}
+          <img src="adobe-686928770-700x200.png" alt="Brad Ahn Law Logo" />
           {/* <h1 className="sitename">Brad Ahn Law</h1> */}
           {/* Your Rights, Our Priority. */}
           {/* Defending. Resolving. Winning. */}
