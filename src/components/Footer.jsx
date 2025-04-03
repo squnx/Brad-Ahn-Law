779c496c235a63512d621d28c5417fb17355f8c3
Ivan Song <mousewurx@gmail.com>
2025-04-03 12:15:50 -0700

    Added 'Attorney Advertising Disclaimer' on the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,23 +7,40 @@ const Footer = () => {
 
     <footer id="footer" className="footer coffee-background">
       <div className="container">
-        <div className="copyright text-center ">
-          <p><span>Copyright</span> © {(new Date().getFullYear())} &nbsp;<strong>Law Offices of Brad J. Ahn</strong>&nbsp; <span>All Rights Reserved.</span></p>
-          {/* <p><span>Designed and Developed by</span> <a href="https://mousewurx.com/" target="_blank" rel="noreferrer"><strong className="px-1 sitename">mousewurx</strong></a></p> */}
+        <div className="row gy-4">
+          <div className="col-lg-6 col-md-12 disclaimer">
+            <span className="sitename">Attorney Advertising Disclaimer</span>
+            <p>The content on this website is for general informational purposes only. Nothing on this site constitutes legal advice for any specific case or situation. Viewing or accessing this information does not establish an attorney-client relationship. For personalized legal counsel, please contact us directly.</p>
+          </div>
+          <div className="col-lg-6 col-md-12">
+            <div className="social-links d-flex justify-content-center">
+              {/* <a href=""><i className="bi bi-twitter-x"></i></a> */}
+              <a href="https://www.google.com/search?q=Law+Offices+of+Brad+J.+Ahn&rlz=1C1GCEU_enUS832US832&oq=Law+Offices+of+Brad+J.+Ahn&aqs=chrome..69i57j69i64j69i61l3&sourceid=chrome&ie=UTF-8#lrd=0x80c2b89b329d2f5b:0x18c7140821663c73,3" title="Google" target="_blank" rel="noreferrer"><i className="bi bi-google"></i></a>
+              <a href="https://www.yelp.com/biz/law-offices-of-brad-j-ahn-fullerton-3" title="Yelp" target="_blank" rel="noreferrer"><i className="bi bi-yelp"></i></a>
+              <a href="https://www.youtube.com/shorts/RqrIaWER0m8" title="Yelp" target="_blank" rel="noreferrer"><i className="bi bi-youtube"></i></a>
+            </div>
+            <div className="copyright text-center ">
+              <p><span>Copyright</span> © {(new Date().getFullYear())} &nbsp;<strong>Law Offices of Brad J. Ahn</strong>&nbsp; <span>All Rights Reserved.</span></p>
+            </div>
+            <div className="credits">
+              Powered by <a href="https://mousewurx.com/" target="_blank" rel="noreferrer"><strong>mousewurx</strong></a>
+            </div>
+          </div>
         </div>
+      </div>
+      {/* <div className="container">
         <div className="social-links d-flex justify-content-center">
-          {/* <a href=""><i className="bi bi-twitter-x"></i></a> */}
-          {/* <a href="https://www.facebook.com/story.php?story_fbid=122105813612709092&substory_index=831504905718856&id=61571272787276&sfnsn=mo&mibextid=RUbZ1f&_rdr" title="Facebook" target="_blank" rel="noreferrer"><i className="bi bi-facebook"></i></a> */}
-          {/* <a href="https://www.facebook.com/profile.php?id=61571272787276" title="Facebook" target="_blank" rel="noreferrer"><i className="bi bi-facebook"></i></a> */}
-          {/* <a href="https://www.instagram.com/soco.wm?igsh=MWQ1ZGUxMzBkMA==" title="Instagram" target="_blank" rel="noreferrer"><i className="bi bi-instagram"></i></a> */}
           <a href="https://www.google.com/search?q=Law+Offices+of+Brad+J.+Ahn&rlz=1C1GCEU_enUS832US832&oq=Law+Offices+of+Brad+J.+Ahn&aqs=chrome..69i57j69i64j69i61l3&sourceid=chrome&ie=UTF-8#lrd=0x80c2b89b329d2f5b:0x18c7140821663c73,3" title="Google" target="_blank" rel="noreferrer"><i className="bi bi-google"></i></a>
           <a href="https://www.yelp.com/biz/law-offices-of-brad-j-ahn-fullerton-3" title="Yelp" target="_blank" rel="noreferrer"><i className="bi bi-yelp"></i></a>
           <a href="https://www.youtube.com/shorts/RqrIaWER0m8" title="Yelp" target="_blank" rel="noreferrer"><i className="bi bi-youtube"></i></a>
         </div>
+        <div className="copyright text-center ">
+          <p><span>Copyright</span> © {(new Date().getFullYear())} &nbsp;<strong>Law Offices of Brad J. Ahn</strong>&nbsp; <span>All Rights Reserved.</span></p>
+        </div>
         <div className="credits">
           Powered by <a href="https://mousewurx.com/" target="_blank" rel="noreferrer"><strong>mousewurx</strong></a>
         </div>
-      </div>
+      </div> */}
     </footer>
   )
 }
